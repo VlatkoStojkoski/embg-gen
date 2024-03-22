8ffce99c03c29cda4c6b9e058431c6144b36c787
Vlatko Stojkoski <stojvlatko@gmail.com>
2024-03-22 22:29:47 +0100

    add reverse generate

--- public/script.js
+++ public/script.js
@@ -20,18 +20,14 @@ const validate = (inputs) => {
 	// date validation
 	if (inputs.date.value === '') {
 		errors.push('Date is required');
-	}
-
-	if (!new RegExp(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/).test(inputs.date.value)) {
+	} else if (!new RegExp(/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/).test(inputs.date.value)) {
 		errors.push('Date must be in correct format');
 	}
 
 	// city validation
 	if (inputs.city.value === '') {
 		errors.push('City is required');
-	}
-
-	if (inputs.city.value.length !== 2 || (+inputs.city.value) < 41 || (+inputs.city.value) > 49) {
+	} else if (inputs.city.value.length !== 2 || (+inputs.city.value) < 41 || (+inputs.city.value) > 49) {
 		errors.push('City must be a valid Macedonian city');
 	}
 
@@ -43,13 +39,22 @@ const validate = (inputs) => {
 	// sex validation
 	if (inputs.sex.value === '') {
 		errors.push('Sex is required');
-	}
-
-	if (!['male', 'female'].includes(inputs.sex.value)) {
+	} else if (!['male', 'female'].includes(inputs.sex.value)) {
 		errors.push('Sex must be either male or female');
 	}
 
 	return errors;
+}
+
+const SELECTORS = {
+	DATE: '#date',
+	CITY: '#city',
+	ORDER: '#order',
+	CHECKED_SEX: 'input[name="sex"]:checked',
+	SEX_FEMALE: '#sex-female',
+	SEX_MALE: '#sex-male',
+	ERROR_CONTAINER: '#error-cards',
+	ERROR_CONTAINER_EMBG: '#error-cards-embg',
 }
 
 const newErrorCard = (message) => {
@@ -60,10 +65,12 @@ const newErrorCard = (message) => {
 	return card;
 }
 
+
 let errorsTimeot = -1;
 
-const setErrors = (errors) => {
-	const errorContainer = document.querySelector('#error-cards');
+const setErrors = (errors, _errorContainer) => {
+	const errorContainer = _errorContainer || document.querySelector(SELECTORS.ERROR_CONTAINER);
+	console.log({ errorContainer, _errorContainer });
 	errorContainer.innerHTML = '';
 
 	if (errors.length > 0) {
@@ -97,10 +104,10 @@ embgDataForm.addEventListener('submit', async (e) => {
 	setErrors([]);
 
 	const inputs = {
-		date: document.querySelector('#date'),
-		city: document.querySelector('#city'),
-		order: document.querySelector('#order'),
-		sex: document.querySelector('input[name="sex"]:checked'),
+		date: document.querySelector(SELECTORS.DATE),
+		city: document.querySelector(SELECTORS.CITY),
+		order: document.querySelector(SELECTORS.ORDER),
+		sex: document.querySelector(SELECTORS.CHECKED_SEX),
 	};
 
 	const errors = validate(inputs);
@@ -126,4 +133,68 @@ embgDataForm.addEventListener('submit', async (e) => {
 	embgOutput.value = res;
 
 	// console.log({ res, embgData });
+});
+
+const embgNumberForm = document.querySelector('#embg-number-form');
+
+const validateEmbg = (embg) => {
+	const errors = [];
+
+	if (embg.length !== 13) {
+		errors.push('EMBG must be 13 characters long');
+	} else if (!new RegExp(/^[0-9]{13}$/).test(embg)) {
+		errors.push('EMBG must contain only numbers');
+	}
+
+	return errors;
+}
+
+const getDataFromEmbg = (embg) => {
+	const dateArr = embg.substring(0, 7).split('');
+	let date = [dateArr[4] + dateArr[5] + dateArr[6], dateArr[2] + dateArr[3], dateArr[0] + dateArr[1]];
+	if (+date[0] < 30) {
+		date[0] = '2' + date[0];
+	} else {
+		date[0] = '1' + date[0];
+	}
+	date = date.join('-');
+
+	const city = embg.substring(7, 9);
+
+	const order = embg.substring(9, 12);
+	const sex = +order < 500 ? 'male' : 'female';
+
+	return {
+		date,
+		city,
+		order,
+		sex,
+	};
+}
+
+embgNumberForm.addEventListener('submit', async (e) => {
+	e.preventDefault();
+
+	setErrors([], document.querySelector(SELECTORS.ERROR_CONTAINER_EMBG));
+
+	const embg = document.querySelector('#embg-number').value;
+
+	const errors = validateEmbg(embg);
+
+	if (errors.length > 0) {
+		setErrors(errors, document.querySelector(SELECTORS.ERROR_CONTAINER_EMBG));
+		return;
+	}
+
+	const res = getDataFromEmbg(embg);
+
+	document.querySelector(SELECTORS.DATE).value = res.date;
+	document.querySelector(SELECTORS.CITY).value = res.city;
+	document.querySelector(SELECTORS.ORDER).value = res.order;
+
+	if (res.sex === 'male') {
+		document.querySelector(SELECTORS.SEX_MALE).checked = true;
+	} else {
+		document.querySelector(SELECTORS.SEX_FEMALE).checked = true;
+	}
 });
